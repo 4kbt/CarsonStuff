943e4c3261b1868183d3b585d6b1ca805c9be8f1
Charlie Hagedorn <cah49@uw.edu>
2017-06-12 14:24:15 -0700

    Adds a pressure-data exercise

---
+++ importUWAtmosData.m
@@ -0,0 +1,18 @@
+d = load('UWAtmosPressure.dat');
+
+%Swap from Julian date to seconds
+d(:,1) = d(:,1).* 86400;
+
+%Switch to seconds since the beginning of the record. 
+d(:,1) = d(:,1) - d(1,1);
+
+%Data record includes a few gaps. This is a crude way to fill them in 
+%in order to make a regular FFT possible.
+
+%make some time, regularly sampled
+t = d(1,1):60:d(end,1); t = t';
+
+%interpolate the gaps, using the nearest actual sample.
+dInterp = interp1(d(:,1), d(:,2), t, 'nearest');
+
+
